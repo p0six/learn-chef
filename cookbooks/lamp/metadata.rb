49ae66c1be72944cd4526819d5ee5f5cd8044bed
Michael Romero <romerom@gmail.com>
2018-08-19 15:22:18 -0700

    Learn Chef Rally - Writing Cookbooks / Create a web app cookbook - complete

--- cookbooks/lamp/metadata.rb
+++ cookbooks/lamp/metadata.rb
@@ -7,17 +7,11 @@ long_description 'Installs/Configures lamp'
 version '0.1.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/lamp/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/lamp'
+issues_url 'https://github.com/p0six/learn-chef/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/p0six/learn-chef' if respond_to?(:source_url)
+supports 'ubuntu'
 
 depends 'httpd', '~> 0.6'
-supports 'ubuntu'
+depends 'mysql', '~> 8.5'
+depends 'mysql2_chef_gem', '~> 2.1'
+depends 'database', '~> 6.1'
---
+++ cookbooks/lamp_customers/metadata.rb
@@ -0,0 +1,14 @@
+name 'lamp_customers'
+maintainer 'Michael Romero'
+maintainer_email 'romerom@gmail.com'
+license 'MIT'
+description 'Installs/Configures lamp_customers'
+long_description 'Installs/Configures lamp_customers'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+issues_url 'https://github.com/p0six/learn-chef/issues'
+source_url 'https://github.com/p0six/learn-chef'
+
+supports 'ubuntu'
+depends 'lamp'
