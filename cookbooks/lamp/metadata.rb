bf1d0e6f53021990204c981ee1f7bf4ba1e7e9d0
Michael Romero <romerom@gmail.com>
2018-08-19 13:36:51 -0700

    Beginning of LAMP cookbook.

---
+++ cookbooks/lamp/metadata.rb
@@ -0,0 +1,23 @@
+name 'lamp'
+maintainer 'Michael Romero'
+maintainer_email 'romerom@gmail.com'
+license 'MIT'
+description 'Installs/Configures lamp'
+long_description 'Installs/Configures lamp'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/lamp/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/lamp'
+
+depends 'httpd', '~> 0.6'
+supports 'ubuntu'
---
+++ cookbooks/lamp/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook:: lamp
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update 'daily' do
+  frequency 86_400
+  action :periodic
+end
